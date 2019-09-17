62162894494c53d3257636de3cc0b52d3c377d6d
Benjamin Langley <blangley@mitre.org>
2019-09-17 09:13:56 -0400

    Websocket UI added

---
+++ src/main/resources/app.js
@@ -0,0 +1,58 @@
+var stompClient = null;
+
+function setConnected(connected) {
+  $("#connect").prop("disabled", connected);
+  $("#disconnect").prop("disabled", !connected);
+  if (connected) {
+    $("#conversation").show();
+  } else {
+    $("#conversation").hide();
+  }
+  $("#messages").html("");
+}
+
+function connect() {
+  socket = new WebSocket("ws://localhost:9000/connect");
+  stompClient = Stomp.over(socket);
+  stompClient.connect({}, function(frame) {
+    setConnected(true);
+    console.log("Connected: " + frame);
+    stompClient.subscribe("/private/notification", function(greeting) {
+      showMessage(greeting.body);
+    });
+  });
+}
+
+function disconnect() {
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+  setConnected(false);
+  console.log("Disconnected");
+}
+
+function bindId() {
+  console.log("binding id");
+  console.log("bind: " + $("#subscriptionId").val());
+  stompClient.send("/subscribe", {}, "bind: " + $("#subscriptionId").val());
+}
+
+function showMessage(message) {
+  $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function() {
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+  });
+  $("#connect").click(function() {
+    connect();
+  });
+  $("#disconnect").click(function() {
+    disconnect();
+  });
+  $("#send").click(function() {
+    console.log("Send clicked");
+    bindId();
+  });
+});
